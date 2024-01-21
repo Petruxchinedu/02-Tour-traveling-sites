b9f0db917777fbb494a8f9443d972028d502ba3c
Petruxchinedu <dominicpeter494@gmail.com>
2024-01-21 18:26:31 +0100

    tourist site

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+  const removeTour = (id)=>{
+    const deleteTour = tours.filter((tour)=> tour.id !== id);
+    setTours(deleteTour)
+  }
+  const fetchData = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <main>
+          <Loading />
+        </main>
+      )
+       : (
+        <main>
+          <Tours tours={tours} removeTour= {removeTour}/>
+        </main>
+      )}
+      {tours.length === 0 && <main>
+        <div className="title">
+          <h4>no tours left</h4>
+          <button onClick={()=>fetchData()}>Refesh</button>
+        </div>
+        </main>}
+    </div>
+  );
+}
+
+export default App;
